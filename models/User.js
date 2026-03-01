687a3410ed9018afae672185eab809455b678440
Avichal296 <avichalshrivastava2006@gmail.com>
2026-03-01 11:15:02 +0530

    added schema

---
+++ models/User.js
@@ -0,0 +1,63 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const bycrypt = require("bcrypt");
+const User = new Schema({
+    name:{
+        type: String,
+        required: true,
+        unique: true
+    },
+    email:{
+        type: String,
+        required:true,
+        unique: true
+    },
+    password:{
+        type: String,
+        required: trusted,
+    },
+    role:{
+        type: Schema.Types.ObjectId,
+        ref: "Role",
+        
+    
+    },
+    IsActive:{
+        type: Boolean,
+        default: true
+    },
+    lastLogin: Date
+},
+{timestamps:true})
+
+
+ SALT_WORK_FACTOR = 10;
+     
+User.pre('save', function(next) {
+    var user = this;
+
+    // only hash the password if it has been modified (or is new)
+    if (!user.isModified('password')) return next();
+
+    // generate a salt
+    bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+        if (err) return next(err);
+
+        // hash the password using our new salt
+        bcrypt.hash(user.password, salt, function(err, hash) {
+            if (err) return next(err);
+            // override the cleartext password with the hashed one
+            user.password = hash;
+            next();
+        });
+    });
+});
+     
+User.methods.comparePassword = function(candidatePassword, cb) {
+    bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+        if (err) return cb(err);
+        cb(null, isMatch);
+    });
+};
+     
+module.exports = mongoose.model('User', User);
